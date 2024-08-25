be864a24983041532f709255a66524b72e7ee8bb
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-08-25 06:30:03 -0700

    Update main.js

--- 3d-portfolio-main/main.js
+++ 3d-portfolio-main/main.js
@@ -106,16 +106,60 @@ let globalProfile = '';
 //   });
 
 
+// axios.get('https://back-api-mu.vercel.app/api/data')
+//   .then(response => {
+//     console.log('API Response:', response.data);
+//     const data = response.data;
+//     const { projects: fetchedProjects, wallname: fetchedWallname } = data;
+
+//     // Update global variables with wallname data
+//     Name = fetchedWallname?.name || '';
+//     About = fetchedWallname?.about || '';
+//     globalProfile = fetchedWallname?.profile ? `https://back-api-mu.vercel.app/textures/${fetchedWallname.profile.split('/').pop()}` : '';
+
+//     console.log('Global Name:', Name);
+//     console.log('Global About:', About);
+//     console.log('Global Profile Image:', globalProfile);
+
+//     // Populate the global projects array
+//     projects = [
+//       {
+//         image: fetchedProjects?.project1Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project1Image.split('/').pop()}` : '',
+//         url: fetchedProjects?.link1 || '',
+//       },
+//       {
+//         image: fetchedProjects?.project2Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project2Image.split('/').pop()}` : '',
+//         url: fetchedProjects?.link2 || '',
+//       },
+//       {
+//         image: fetchedProjects?.project3Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project3Image.split('/').pop()}` : '',
+//         url: fetchedProjects?.link3 || '',
+//       },
+//       {
+//         image: fetchedProjects?.project4Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project4Image.split('/').pop()}` : '',
+//         url: fetchedProjects?.link4 || '',
+//       },
+//     ];
+
+//     console.log('Projects Array:', projects);
+//     ProjectTextures(); // Initialize project textures with fetched data
+//   })
+//   .catch(error => {
+//     console.error('There was an error fetching the data!', error);
+//   });
 axios.get('https://back-api-mu.vercel.app/api/data')
   .then(response => {
     console.log('API Response:', response.data);
     const data = response.data;
-    const { projects: fetchedProjects, wallname: fetchedWallname } = data;
+    const { projects: fetchedProjects = {}, wallname: fetchedWallname = {} } = data;
+
+    // Base URL for your GitHub repository raw files
+    const githubBaseUrl = 'https://raw.githubusercontent.com/Amponsah-Boahen-Denis/imagesfiles/main/textures/';
 
     // Update global variables with wallname data
-    Name = fetchedWallname?.name || '';
-    About = fetchedWallname?.about || '';
-    globalProfile = fetchedWallname?.profile ? `https://back-api-mu.vercel.app/textures/${fetchedWallname.profile.split('/').pop()}` : '';
+    Name = fetchedWallname.name || '';
+    About = fetchedWallname.about || '';
+    globalProfile = fetchedWallname.profile ? `${githubBaseUrl}${fetchedWallname.profile.split('/').pop()}` : '';
 
     console.log('Global Name:', Name);
     console.log('Global About:', About);
@@ -124,20 +168,20 @@ axios.get('https://back-api-mu.vercel.app/api/data')
     // Populate the global projects array
     projects = [
       {
-        image: fetchedProjects?.project1Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project1Image.split('/').pop()}` : '',
-        url: fetchedProjects?.link1 || '',
+        image: fetchedProjects.project1Image ? `${githubBaseUrl}${fetchedProjects.project1Image.split('/').pop()}` : '',
+        url: fetchedProjects.link1 || '',
       },
       {
-        image: fetchedProjects?.project2Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project2Image.split('/').pop()}` : '',
-        url: fetchedProjects?.link2 || '',
+        image: fetchedProjects.project2Image ? `${githubBaseUrl}${fetchedProjects.project2Image.split('/').pop()}` : '',
+        url: fetchedProjects.link2 || '',
       },
       {
-        image: fetchedProjects?.project3Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project3Image.split('/').pop()}` : '',
-        url: fetchedProjects?.link3 || '',
+        image: fetchedProjects.project3Image ? `${githubBaseUrl}${fetchedProjects.project3Image.split('/').pop()}` : '',
+        url: fetchedProjects.link3 || '',
       },
       {
-        image: fetchedProjects?.project4Image ? `https://back-api-mu.vercel.app/textures/${fetchedProjects.project4Image.split('/').pop()}` : '',
-        url: fetchedProjects?.link4 || '',
+        image: fetchedProjects.project4Image ? `${githubBaseUrl}${fetchedProjects.project4Image.split('/').pop()}` : '',
+        url: fetchedProjects.link4 || '',
       },
     ];
 
