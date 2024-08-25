9d792e48f57397095925f82e79b6daf712738a1d
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-08-25 02:51:09 -0700

    Update index.js

--- 3d-backend/index.js
+++ 3d-backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
