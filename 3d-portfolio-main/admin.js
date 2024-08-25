3ed7ee6b59569b7c80b00c55d14dd77600d8b1bd
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-08-25 04:16:21 -0700

    Update admin.js

--- 3d-portfolio-main/admin.js
+++ 3d-portfolio-main/admin.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('name').value = wallname.name || '';
                 document.getElementById('about').value = wallname.about || '';
                 if (wallname.profile) {
-                    document.getElementById('profilePreview').src = `/textures/${wallname.profile.split('/').pop()}`;
+                    document.getElementById('profilePreview').src = `https://back-api-mu.vercel.app${wallname.profile}`;
                 }
             }
 
@@ -32,16 +32,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('link4').value = projects.link4 || '';
 
                 if (projects.project1Image) {
-                    document.getElementById('project1Preview').src = `https://back-api-mu.vercel.app/textures/${projects.project1Image.split('/').pop()}`;
+                    document.getElementById('project1Preview').src = `https://back-api-mu.vercel.app${projects.project1Image}`;
                 }
                 if (projects.project2Image) {
-                    document.getElementById('project2Preview').src = `https://back-api-mu.vercel.app/textures/${projects.project2Image.split('/').pop()}`;
+                    document.getElementById('project2Preview').src = `https://back-api-mu.vercel.app${projects.project2Image}`;
                 }
                 if (projects.project3Image) {
-                    document.getElementById('project3Preview').src = `https://back-api-mu.vercel.app/textures/${projects.project3Image.split('/').pop()}`;
+                    document.getElementById('project3Preview').src = `https://back-api-mu.vercel.app${projects.project3Image}`;
                 }
                 if (projects.project4Image) {
-                    document.getElementById('project4Preview').src = `https://back-api-mu.vercel.app/textures/${projects.project4Image.split('/').pop()}`;
+                    document.getElementById('project4Preview').src = `https://back-api-mu.vercel.app${projects.project4Image}`;
                 }
             }
         })
